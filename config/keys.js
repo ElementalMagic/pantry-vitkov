e32361a4ac4266902572053b0deeaeb5ecb86fde
Alex Vahtin <vkstrfrt@gmail.com>
2018-11-22 19:16:03 +0300

    'keysFix'

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-    mongoURI: false ? 'mongodb://localhost:27017' :'mongodb://alexandr:nhbnjy89@ds123963.mlab.com:23963/fullstack' ,
+
+    mongoURI: process.env.USELOCALURI || false ? 'mongodb://localhost:27017' :'mongodb://alexandr:nhbnjy89@ds123963.mlab.com:23963/fullstack' ,
     jwt: 'dev-jwt'
 };
 
