21b356ad343d5672f4f09db67bca2e0a73ba5e7f
Alex Vahtin <vkstrfrt@gmail.com>
2018-11-26 15:49:21 +0300

    testing dynamic meta tags

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -3,6 +3,7 @@ import {Observable} from "rxjs";
 import {ProductsService} from "../shared/services/products.service";
 import {Collection, ProductType} from "../shared/interfaces";
 import LazyLoad from "vanilla-lazyload";
+import {Meta, Title} from "@angular/platform-browser";
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -12,10 +13,15 @@ export class MainComponent implements OnInit {
   collections: Collection[];
   mainCollection: Collection;
   mColLoaded = false;
-  constructor(private mainService: ProductsService) {
+  constructor(private mainService: ProductsService, private meta: Meta, private title: Title) {
   }
 
   ngOnInit() {
+    this.title.setTitle('Vitkov Pantry - Handmade продукция');
+    this.meta.addTag({name: 'description', content: 'Коллекции сувенирной продукции «PANTRY VITKOV», созданные в сотрудничестве с российскими художниками.\n' +
+        'В ассортименте: наборы подставок под посуду, подставки под чашки и бокалы из картона и пробкового полотна, блокноты, скетчбуки.\n' +
+        'Продукция изготавливается на собственном производстве, которое находится в Московской области.\n' +
+        'Качество продукции соответствует аналогам европейского производства.'});
     var myLazyLoad = new LazyLoad({
       elements_selector: ".lazy"
     });
